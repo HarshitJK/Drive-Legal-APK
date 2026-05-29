3bb7ca95cafb90769440b4b1d8e16c63b13f3e9b
Barathvs18 <vsbarath095@gmail.com>
2026-05-29 18:07:34 +0530

    Initial commit with backend folder structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Demo } from "./components/demo"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#030303] text-white flex items-center justify-center">
+      <Demo />
+    </div>
+  )
+}
+
+export default App
